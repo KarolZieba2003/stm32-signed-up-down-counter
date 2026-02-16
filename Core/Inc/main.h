590cc8fff386be3f6b7412b0339e3984a7954136
Karol Zieba <karol.zieba_inbox@proton.me>
2026-02-16 08:22:17 +0100

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,98 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define interrupt_13_Pin GPIO_PIN_13
+#define interrupt_13_GPIO_Port GPIOC
+#define interrupt_13_EXTI_IRQn EXTI15_10_IRQn
+#define interrupt_0_Pin GPIO_PIN_0
+#define interrupt_0_GPIO_Port GPIOC
+#define interrupt_0_EXTI_IRQn EXTI0_IRQn
+#define interrupt_1_Pin GPIO_PIN_1
+#define interrupt_1_GPIO_Port GPIOC
+#define interrupt_1_EXTI_IRQn EXTI1_IRQn
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define sign_pin_Pin GPIO_PIN_4
+#define sign_pin_GPIO_Port GPIOA
+#define MSB_Pin GPIO_PIN_5
+#define MSB_GPIO_Port GPIOA
+#define second_bit_pin_Pin GPIO_PIN_6
+#define second_bit_pin_GPIO_Port GPIOA
+#define LSB_Pin GPIO_PIN_7
+#define LSB_GPIO_Port GPIOA
+#define third_bit_pin_Pin GPIO_PIN_4
+#define third_bit_pin_GPIO_Port GPIOC
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
